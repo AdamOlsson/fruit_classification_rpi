72baf16973bc7463c857336f66174cf2ca318275
AdamOlsson <Olsson.95_@hotmail.com>
2018-04-03 21:42:52 +0200

    Added translation dict

--- backend/lib/classifier.py
+++ backend/lib/classifier.py
@@ -4,6 +4,19 @@ import os
 
 class Classifier:
 
+    translations = {
+        "apple": u"Äpple",
+        "banana" : u"Banan",
+        "kiwi": u"Kiwi",
+        "potato": u"Potato",
+        "bell pepper": u"Paprika",
+        "orange": u"Apelsin,
+        "clementine": u"Klementin",
+        "tomato": u"Tomat",
+        "avocado": u"Avokado",
+        "pear": u"Päron"
+    }
+
     def __init__(self, graph_file, label_file, input_name, output_name, net="mobilenet"):
         self.graph = self.load_graph(graph_file)
         self.labels = self.load_labels(label_file)
@@ -36,7 +49,7 @@ class Classifier:
         top_k = results.argsort()[-5:][::-1]
         res_list = []
         for i in top_k:
-            res_list.append({"object": self.labels[i], "accuracy":str(results[i])})
+            res_list.append({"object": translations[self.labels[i]], "accuracy":str(results[i])})
         return res_list
 
 
