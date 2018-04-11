246ae438cbdd5696ff64e471565bc86a344ae505
System Administrator <root@Fridas-MBP-3.lan>
2018-04-11 09:15:56 +0200

    Frontend - changing height of search field at identification page

--- frontend/src/pages/IdentificationResults.js
+++ frontend/src/pages/IdentificationResults.js
@@ -76,7 +76,7 @@ class IdentificationResults extends React.Component {
                     <div className="panel" style={{ flex: 1 }} >
                         <span> Välj önskad vara </span>
                     </div>
-                    <div style={{ justifyContent: 'center', alignItems: 'center', maxHeight: '45%' }}>
+                    <div style={{ justifyContent: 'center', alignItems: 'center', maxHeight: '55%' }}>
                         <div>
                             {filteredProducts.map((product) => {
                                 return (<ProductItem
