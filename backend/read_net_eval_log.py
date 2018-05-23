ac9e680a6f4e9382980ebcbe29d8c4c8a5c5eaf0
AdamOlsson <Olsson.95_@hotmail.com>
2018-05-23 20:48:54 +0200

    added script to calibrate HX711

--- backend/read_net_eval_log.py
+++ backend/read_net_eval_log.py
@@ -34,15 +34,28 @@ try:
             #fruit = fruit.split("\n")[0] # Remove the '\n' at the end of string for pretty print purposes
 
             inception = inception.replace("'", "\"") # " cannot be ' for to be read as json
-            mobilenet = mobilenet.replace("'", "\"")            
+            mobilenet = mobilenet.replace("'", "\"")
+
+            inception = inception.replace("\\xc4", "a")
+            inception = inception.replace("\\xe4", "a")
+            mobilenet = mobilenet.replace("\\xc4", "a")
+            mobilenet = mobilenet.replace("\\xe4", "a")
+            inception = inception.replace("u", "")
+            mobilenet = mobilenet.replace("u", "")
+            inception = inception.replace("accracy", "accuracy")
+            mobilenet = mobilenet.replace("accracy", "accuracy")
+
 
             print inception
             inception_data = json.loads(inception)
             mobilenet_data = json.loads(mobilenet)
 
             print 'Image {:<3} {:>25} || {:<22} Correct answer: {}'.format(line_num, "Inception LEFT", "Mobilenet RIGHT", fruit)
-            for item in range(len(inception_data)): # lengths are the same
-                print '{}. {:<11} {:<20} ||         {}. {:<11} {:<30}'.format(item, inception_data[item]["object"], inception_data[item]["accuracy"], item, mobilenet_data[item]["object"], mobilenet_data[item]["accuracy"])
+            with open("dummy.txt", "a+") as dummy:
+                for item in range(len(inception_data)): # lengths are the same
+                    print '{}. {:<11} {:<20} ||         {}. {:<11} {:<30}'.format(item, inception_data[item]["object"], inception_data[item]["accuracy"], item, mobilenet_data[item]["object"], mobilenet_data[item]["accuracy"])
+                    dummy.write(mobilenet_data[item]["object"] + "\n")
+                dummy.write("\n")
             
             print '{} {} {:<10} {:<10} {} {} {}'.format("Inception:", inception_prop_time, "seconds", "||","Mobilenet:", mobilenet_prop_time, "seconds")
 
